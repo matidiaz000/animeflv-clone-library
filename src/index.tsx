8cedb13f27c3941204ff1a7e519f37378b516027
Matias Díaz <matidiaz00@gmail.com>
2025-04-19 20:42:15 -0300

    try to update package version in cicd

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import './index.scss'
+import IconsList from "./icomoon/selection.json";
 import { Button } from "./components/Button";
 import { Icon } from "./components/Icon";
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
-export { Button, Icon, Card, Header }
+export { Button, Icon, Card, Header, IconsList }
 export type { IColors, IColorsBasics, IColorsGray, IColorsPrimary, IColorsSecondary } from "./inrerfaces/Colors";
