981e4076c8858c317dc30f597c7f8c1771abbc1b
Matias Díaz <matidiaz00@gmail.com>
2025-04-21 19:28:05 -0300

    problem with selection json file from icomoon

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,20 +3,6 @@ import { defineConfig } from '@rslib/core';
 import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
-  tools: {
-    rspack(config, { addRules }) {
-      addRules([
-        {
-          test: /\.json$/,
-          // Convert assets to separate files and keep import statements
-          type: 'asset/resource',
-          generator: {
-            importMode: 'preserve',
-          },
-        },
-      ]);
-    },
-  },
   source: {
     entry: {
       index: ['./src/**'],
