881bc1a1a19e86ecaded3e3377ddf891e71a45e7
Matias Díaz <matidiaz00@gmail.com>
2025-05-02 20:47:08 -0300

    change buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import type { IColors } from "../inrerfaces/Colors";
 import { Icon } from "./Icon";
 
 interface IProps {
-  variant: 'text' | 'contained' | 'outlined' | 'watercolor'
+  variant: 'text' | 'contained' | 'outlined'
   children?: React.ReactNode,
   href?: string,
   reload?: boolean,
@@ -13,7 +13,7 @@ interface IProps {
   size?: 'sm' | 'md' | 'lg',
   startIcon?: string,
   endIcon?: string,
-  className: string
+  className?: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [x: string]: any;
 }
@@ -21,19 +21,18 @@ interface IProps {
 export const Button = ({variant, children, href, external, disabled, color, span, size, startIcon, endIcon, className, ...rest}: IProps) => {
   const getClass = (): string => {
     const listClass: string[] = [
-      (span ? 'px-2 py-1' : 'btn'), 
-      'text-decoration-none',
-      (!className.includes("rounded") ? "rounded-pill" : ""),
+      (span ? 'px-2 py-1' : 'btn'),
+      'text-decoration-none text-uppercase',
+      (!className?.includes("rounded") ? "rounded-pill" : ""),
       (disabled ? `disabled` : ""),
       (color && variant == 'text' ? `text-${color} border-0` : ""),
       (color && variant == 'contained' ? `btn-${color}` : ""),
       (color && variant == 'outlined' ? `btn-outline-${color}` : ""),
-      (color && variant == 'watercolor' ? `watercolor-${color}` : ""),
       (size == 'sm' ? `btn-sm` : ""),
       (size == 'md' ? `` : ""),
       (size == 'lg' ? `btn-lg` : ""),
       (startIcon && !children && !endIcon ? "rounded-circle border-0 lh-0 p-2" : ""),
-      className
+      (className ? className : "")
     ];
     return listClass.join(" ");
   };
