83acd1b4e55b5d5cbd41a8c4ad37ce93cb6d414f
Matias Díaz <matidiaz00@gmail.com>
2025-05-04 17:49:12 -0300

    responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,15 @@ interface IProps {
 export const Header = ({logo, leftContent, rightContent}: IProps) => {
   return (
     <header className="bg-dark">
-      <div className="container py-4">
+      <div className="container-md py-4">
         <div className="d-flex justify-content-between align-items-center">
           <a href="/">
             <img src={logo} alt="AnimeFLV" />
           </a>
-          <nav className="d-flex align-items-center ms-3">
+          <nav className="d-flex align-items-center ms-md-3 ms-auto">
             {leftContent}
           </nav>
-          <nav className="ms-auto d-flex align-items-center me-n3">
+          <nav className="ms-auto d-none d-md-flex align-items-center me-n3">
             {rightContent}
           </nav>
         </div>
