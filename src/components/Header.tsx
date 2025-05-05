13676895673f220db6fb50b0bf015b20cb055d6e
Matias Díaz <matidiaz00@gmail.com>
2025-05-04 22:07:43 -0300

    add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ interface IProps {
 export const Header = ({logo, leftContent, rightContent}: IProps) => {
   return (
     <header className="bg-dark">
-      <div className="container-md py-4">
+      <div className="container-md py-3 py-md-4">
         <div className="d-flex justify-content-between align-items-center">
           <NavLink to="/">
             <img src={logo} alt="AnimeFLV" />
