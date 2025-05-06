d32083f9dbf63c4c85e0dc0f1081a580ee741f24
Matias Díaz <matidiaz00@gmail.com>
2025-05-06 20:37:58 -0300

    main version

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { IconsList } from "./utilities/Selection";
 import { Button } from "./components/Button";
 import { Icon } from "./components/Icon";
 import { Card } from "./components/Card";
-import { Header } from "./components/Header";
-export { Button, Icon, Card, Header, IconsList };
+
+export { Button, Icon, Card, IconsList };
 export type { IColors, IColorsBasics, IColorsGray, IColorsPrimary, IColorsSecondary } from "./inrerfaces/Colors";
